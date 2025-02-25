903051510c3a49af11ca5e0ca303989601775f1c
denden0121 <denferjerichomagoabad@gmail.com>
2025-02-25 21:32:35 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,33 @@ import Paralax from './Sections/Parallax.jsx';
 import DividerX from './Extra_Components/DividerX.jsx';
 
 function App() {
-  
-  return (
-    <div className="overflow-hidden w-full relative flex md:flex-row md:justify-between md:items-start flex-col justify-start items-start">
-      {/* 1st column */}
-      <div className="md:w-[25%] w-[0%] h-[0] md:sticky md:top-0">
-        <Navigations/>
-      </div>
-      {/* 2nd column */}
-      <div className="md:w-[50%] w-[100%] ">
-        <Header/>
-        <DividerX/>
-        <Stack/>
-        <DividerX/>
-        <Projects/>
-        <DividerX/>
-        <Contacts/>
-      </div>
-      {/* 3rd column */}
-      <div className="md:w-[25%] w-[0%]">
-      </div>
-      
-        <span id='noiseBg'></span>
-    </div>
-  );
-  
-  const projectSection = document.getElementById('contact-section');
+
+	return (
+		<div className="overflow-hidden w-full relative flex md:flex-row md:justify-between md:items-start flex-col justify-start items-start">
+			
+			{/* 1st column */}
+			<div className="md:w-[25%] w-[0%] h-[0] md:sticky md:top-0">
+				<Navigations/>
+			</div>
+			
+			{/* 2nd column */}
+			<div className="md:w-[50%] w-[100%] ">
+				<Header/>
+				<DividerX/>
+				<Stack/>
+				<DividerX/>
+				<Projects/>
+				<DividerX/>
+				<Contacts/>
+			</div>
+			
+			{/* 3rd column */}
+			<div className="md:w-[25%] w-[0%]"></div>
+
+			<span id='noiseBg'></span>
+		</div>
+	);
+
 }
 
 export default App;
